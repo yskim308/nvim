2cb5d96e3cec24448eafbc9425fa216a6b444b94
yskim308 <youngseokim7@gmail.com>
2024-03-25 00:52:11 +0900

    add line number to nvim-tree and install telescope

--- lua/yskim/init.lua
+++ lua/yskim/init.lua
@@ -11,3 +11,8 @@ vim.opt.number = true
 vim.opt.relativenumber = true
 
 vim.opt.clipboard = "unnamedplus"
+
+vim.keymap.set("n", "<C-h>", "<C-w>h")
+vim.keymap.set("n", "<C-j>", "<C-w>j")
+vim.keymap.set("n", "<C-k>", "<C-w>k")
+vim.keymap.set("n", "<C-l>", "<C-w>l")
--- lua/yskim/plugins/nvim-tree.lua
+++ lua/yskim/plugins/nvim-tree.lua
@@ -4,8 +4,7 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function ()
-        local nvimtree = require("nvim-tree")
-        local api = require "nvim-tree.api"
+        local nvimtree = require("nvim-tree").setup({view = {relativenumber = true}})
         -- disable netrw at the very start of your init.lua
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
@@ -13,9 +12,12 @@ return {
         -- optionally enable 24-bit colour
         vim.opt.termguicolors = true
 
+        vim.opt.number = true
+        --line numbers
         -- empty setup using defaults
         require("nvim-tree").setup()
         vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
         vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>") 
+
     end
 }
