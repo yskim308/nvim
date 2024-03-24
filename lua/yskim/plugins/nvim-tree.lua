71251287c71ebc27b41480bf1e819fdbaeab4c12
yskim308 <youngseokim7@gmail.com>
2024-03-25 01:06:04 +0900

    add relative numbers to nvimtree

--- lua/yskim/plugins/nvim-tree.lua
+++ lua/yskim/plugins/nvim-tree.lua
@@ -4,7 +4,7 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     config = function ()
-        local nvimtree = require("nvim-tree").setup({view = {relativenumber = true}})
+        local nvimtree = require("nvim-tree")
         -- disable netrw at the very start of your init.lua
         vim.g.loaded_netrw = 1
         vim.g.loaded_netrwPlugin = 1
@@ -12,10 +12,9 @@ return {
         -- optionally enable 24-bit colour
         vim.opt.termguicolors = true
 
-        vim.opt.number = true
         --line numbers
         -- empty setup using defaults
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({view = {relativenumber = true}})
         vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
         vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>") 
 
