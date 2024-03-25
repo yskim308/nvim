6d98f9f0667eee5cf1f1799f968b01454687d5a3
yskim308 <youngseokim7@gmail.com>
2024-03-25 22:36:51 +0900

    change mapping for nvim-cmp

--- lua/yskim/plugins/nvim-cmp.lua
+++ lua/yskim/plugins/nvim-cmp.lua
@@ -29,8 +29,8 @@ return {
         end,
       },
       mapping = cmp.mapping.preset.insert({
-        ["<Tab>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-        ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
+        ["<C-k"] = cmp.mapping.select_prev_item(), -- previous suggestion
+        ["<Tab>"] = cmp.mapping.select_next_item(), -- next suggestion
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
