bd95080e86bbff952abbbe93a1b7ef4d31925af8
yskim308 <youngseokim7@gmail.com>
2024-06-12 13:38:09 +0900

    set lightmode and uhhh idk what the other changes are lol

--- lua/yskim/plugins/autopairs.lua
+++ lua/yskim/plugins/autopairs.lua
@@ -5,4 +5,3 @@ return {
     -- use opts = {} for passing setup options
     -- this is equalent to setup({}) function
 }
-
--- lua/yskim/plugins/colorscheme.lua
+++ lua/yskim/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     priority = 1000,
     config = function()
         require("gruvbox").setup()
-        vim.o.background = "dark"
+        vim.o.background = "light"
         vim.cmd('colorscheme gruvbox')
     end
 }
