938ef4d7bdb9d8e5262705b8a4a0dac4093e2b43
yskim308 <youngseokim7@gmail.com>
2024-06-12 13:54:03 +0900

    revert to dark gruvbox

--- lua/yskim/plugins/colorscheme.lua
+++ lua/yskim/plugins/colorscheme.lua
@@ -3,7 +3,7 @@ return {
     priority = 1000,
     config = function()
         require("gruvbox").setup()
-        vim.o.background = "light"
+        vim.o.background = "dark"
         vim.cmd('colorscheme gruvbox')
     end
 }
