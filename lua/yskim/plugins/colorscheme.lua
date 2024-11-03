9c74f02122c1174d6f725073d9ee1c1191953754
Young Seo <youngseokim7@gmail.com>
2024-11-03 15:55:02 -0800

    chnage colorscheme to rose-pine :)

--- lua/yskim/plugins/colorscheme.lua
+++ lua/yskim/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    'shaunsingh/nord.nvim',
+    'rose-pine/neovim',
     --'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
         --require("nord").setup()
         --require("gruvbox").setup()
         --vim.o.background = "dark"
-        vim.cmd('colorscheme nord')
+        vim.cmd('colorscheme rose-pine')
         --vim.cmd('colorscheme gruvbox')
     end
 }
