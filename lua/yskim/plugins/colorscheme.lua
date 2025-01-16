9525237b6d2767b86eebe154b80855b407b5b175
Young Seo Kim <young@y.local>
2025-01-16 18:17:20 +0900

    revert colorscheme to gruvbox

--- lua/yskim/plugins/colorscheme.lua
+++ lua/yskim/plugins/colorscheme.lua
@@ -1,14 +1,10 @@
 return {
-    'rose-pine/neovim',
-    --'ellisonleao/gruvbox.nvim',
+    'ellisonleao/gruvbox.nvim',
     priority = 1000,
     config = function()
-        require("rose-pine").setup({
-            variant = "moon",
-        })
-        --require("gruvbox").setup()
+        require("gruvbox").setup()
         --vim.o.background = "dark"
-        vim.cmd('colorscheme rose-pine')
-        --vim.cmd('colorscheme gruvbox')
+        --vim.cmd('colorscheme rose-pine')
+        vim.cmd('colorscheme gruvbox')
     end
 }
