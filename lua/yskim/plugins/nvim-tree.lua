5c91b59fc7d2acf3096f28a75e12a664e7f5d342
Young Seo Kim <youngseokim7@gmail.com>
2025-04-14 12:19:06 +0900

    format nvim-tree config

--- lua/yskim/plugins/nvim-tree.lua
+++ lua/yskim/plugins/nvim-tree.lua
@@ -1,29 +1,28 @@
 return {
-    "nvim-tree/nvim-tree.lua",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons"
-    },
-    config = function ()
-        local nvimtree = require("nvim-tree")
-        -- disable netrw at the very start of your init.lua
-        vim.g.loaded_netrw = 1
-        vim.g.loaded_netrwPlugin = 1
+  "nvim-tree/nvim-tree.lua",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+  config = function()
+    local nvimtree = require("nvim-tree")
+    -- disable netrw at the very start of your init.lua
+    vim.g.loaded_netrw = 1
+    vim.g.loaded_netrwPlugin = 1
 
-        -- optionally enable 24-bit colour
-        vim.opt.termguicolors = true
+    -- optionally enable 24-bit colour
+    vim.opt.termguicolors = true
 
-        --line numbers
-        -- empty setup using defaults
-        require("nvim-tree").setup({
-            view = {
-                relativenumber = true,
-                width = 30,
-                side = 'right'
-            },     
-        })
+    --line numbers
+    -- empty setup using defaults
+    require("nvim-tree").setup({
+      view = {
+        relativenumber = true,
+        width = 30,
+        side = "right",
+      },
+    })
 
-        vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
-        vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>") 
-
-    end
+    vim.keymap.set("n", "<C-n>", "<cmd>NvimTreeToggle<CR>")
+    vim.keymap.set("n", "<leader>e", "<cmd>NvimTreeFocus<CR>")
+  end,
 }
