c85f36e01c80c6f8e5a5f8d38511d745c3fb852a
Young Seo Kim <youngseokim7@gmail.com>
2025-04-20 15:05:02 +0900

    configure prisma lsp

--- lua/yskim/plugins/lsp/mason.lua
+++ lua/yskim/plugins/lsp/mason.lua
@@ -1,36 +1,36 @@
 return {
-    "williamboman/mason.nvim"
-    ,
-    dependencies = {
-        'williamboman/mason-lspconfig.nvim',
-    },
-    config = function()
-        local mason = require("mason")
-        local mason_lspconfig = require("mason-lspconfig")
+  "williamboman/mason.nvim",
+  dependencies = {
+    "williamboman/mason-lspconfig.nvim",
+  },
+  config = function()
+    local mason = require("mason")
+    local mason_lspconfig = require("mason-lspconfig")
 
-        require("mason").setup({
-            ui = {
-                icons = {
-                    package_installed = "✓",
-                    package_pending = "➜",
-                    package_uninstalled = "✗"
-                }
-            }
-        })
+    require("mason").setup({
+      ui = {
+        icons = {
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗",
+        },
+      },
+    })
 
-        mason_lspconfig.setup({
-            ensurts_lsled = {
-                "ts_ls",
-                "html",
-                "cssls",
-                "lua_ls",
-                "tailwindcss",
-                "svelte",
-                "emmet_ls",
-                "pyright",
-                "clangd",
-            },
-            automatic_installation = true,
-        })
-    end
+    mason_lspconfig.setup({
+      ensurts_lsled = {
+        "ts_ls",
+        "html",
+        "cssls",
+        "lua_ls",
+        "tailwindcss",
+        "svelte",
+        "emmet_ls",
+        "pyright",
+        "clangd",
+        "prisma-language-server",
+      },
+      automatic_installation = true,
+    })
+  end,
 }
