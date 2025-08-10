c2be10664e0e9e762d9940e4a5aa2d002aad5bdb
Young Seo Kim <youngseokim7@gmail.com>
2025-08-11 00:44:17 +0900

    add image support

---
+++ lua/yskim/plugins/image.lua
@@ -0,0 +1,14 @@
+return {
+  -- lazy.nvim
+  {
+    "folke/snacks.nvim",
+    ---@type snacks.Config
+    opts = {
+      image = {
+        -- your image configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      },
+    },
+  },
+}
