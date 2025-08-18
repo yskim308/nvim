e39b311808aafa5cf7ce0625b5aacb3cc32e8333
Young Seo Kim <youngseokim7@gmail.com>
2025-08-18 18:50:35 +0900

    update deprecated treesitter method

--- lua/yskim/plugins/nvim-treesitter.lua
+++ lua/yskim/plugins/nvim-treesitter.lua
@@ -2,7 +2,7 @@ return {
   "nvim-treesitter/nvim-treesitter",
   build = ":TSUpdate",
   config = function()
-    local configs = require("nvim-treesitter.configs")
+    local configs = require("nvim-ts-autotag")
     configs.setup({
       ensure_installed = { "c", "lua", "vim", "vimdoc", "cpp", "python", "css", "javascript", "html" },
       sync_install = false,
