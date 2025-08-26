8840b645def3b3ddc615bca753d14979c717a99a
Young Seo Kim <youngseokim7@gmail.com>
2025-08-26 16:49:35 +0900

    make tabs 2 spaces. 4 spaces suck ass

--- lua/yskim/init.lua
+++ lua/yskim/init.lua
@@ -1,8 +1,8 @@
 vim.opt.expandtab = true
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 2
 vim.opt.smartindent = true
-vim.opt.tabstop = 4
-vim.opt.softtabstop = 4
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
 
 vim.g.mapleader = " "
 -- Set leader key
