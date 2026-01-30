2d5518c5079a342031796d988e78cc88146e0ad9
Young Seo Kim <youngseokim7@gmail.com>
2026-01-30 22:58:34 +0900

    change colorscheme to color picker

--- lua/yskim/plugins/colorscheme.lua
+++ lua/yskim/plugins/colorscheme.lua
@@ -1,7 +1,20 @@
 return {
-  "rebelot/kanagawa.nvim",
-  priority = 1000,
+  "flashcodes-themayankjha/fkthemes.nvim",
+  event = "VeryLazy",
+  dependencies = {
+    "MunifTanjim/nui.nvim",
+    "nvim-telescope/telescope.nvim",
+    "rebelot/kanagawa.nvim",
+    "catppuccin/nvim",
+    "ellisonleao/gruvbox.nvim",
+    "tanvirtin/monokai.nvim",
+    -- Add your themes here
+  },
   config = function()
-    require("kanagawa").load()
+    require("fkthemes").setup({
+      themes = { "kanagawa", "catppuccin", "gruvbox", "monokai" },
+      default_theme = "kanagawa",
+      transparent_background = true,
+    })
   end,
 }
