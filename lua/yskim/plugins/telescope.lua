a020619fd67da6bdc789fa1b3c524c838c9f4702
Young Seo Kim <youngseokim7@gmail.com>
2026-01-30 22:29:13 +0900

    add document symbol search and reference seraching in telescope

--- lua/yskim/plugins/telescope.lua
+++ lua/yskim/plugins/telescope.lua
@@ -1,12 +1,15 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.6',
--- or                              , branch = '0.1.x',
-      dependencies = { 'nvim-lua/plenary.nvim' },
-    config = function()
-        local builtin = require('telescope.builtin')
-        vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-        vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
-        vim.keymap.set('n', '<leader>b', builtin.buffers, {})
-        vim.keymap.set('n', '<leader>fh', builtin.help_tags, {})
-    end
-    }
+  "nvim-telescope/telescope.nvim",
+  tag = "0.1.6",
+  -- or                              , branch = '0.1.x',
+  dependencies = { "nvim-lua/plenary.nvim" },
+  config = function()
+    local builtin = require("telescope.builtin")
+    vim.keymap.set("n", "<leader>ff", builtin.find_files, {})
+    vim.keymap.set("n", "<leader>fg", builtin.live_grep, {})
+    vim.keymap.set("n", "<leader>b", builtin.buffers, {})
+    vim.keymap.set("n", "<leader>fh", builtin.help_tags, {})
+    vim.keymap.set("n", "<leader>fr", builtin.lsp_references, {})
+    vim.keymap.set("n", "<leader>a", builtin.lsp_document_symbols, {})
+  end,
+}
