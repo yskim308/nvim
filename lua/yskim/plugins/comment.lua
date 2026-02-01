971c3a868250319226a53a8a01b93f9efb5bb651
Young Seo Kim <youngseokim7@gmail.com>
2026-02-01 17:12:38 +0900

    "remove unused key binding"

--- lua/yskim/plugins/comment.lua
+++ lua/yskim/plugins/comment.lua
@@ -1,15 +1,6 @@
 return {
   "numToStr/Comment.nvim",
   opts = {},
-  keys = {
-    {
-      "<leader>m",
-      function()
-        MiniFiles.open()
-      end,
-      desc = "Open MiniFiles",
-    },
-  },
   config = function()
     require("Comment").setup()
   end,
