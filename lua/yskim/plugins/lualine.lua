758fb077ca7630f47c0e0b561b7f9835244d79a7
Young Seo Kim <youngseokim7@gmail.com>
2026-02-01 16:53:26 +0900

    make lualine dpepend on themery, fix issues with snacks

--- lua/yskim/plugins/lualine.lua
+++ lua/yskim/plugins/lualine.lua
@@ -1,7 +1,9 @@
 return {
   "nvim-lualine/lualine.nvim",
-  dependencies = { "nvim-tree/nvim-web-devicons" },
+  dependencies = { "nvim-tree/nvim-web-devicons", "zaldih/themery.nvim" },
   config = function()
-    require("lualine").setup()
+    require("lualine").setup({ options = {
+      theme = "auto",
+    } })
   end,
 }
