ba496020ed76bfe03918d6ac770a003f23998446
Young Seo Kim <youngseokim7@gmail.com>
2026-03-04 08:32:02 +0900

    add support for generic surrounds

--- lua/yskim/plugins/surround.lua
+++ lua/yskim/plugins/surround.lua
@@ -1,10 +1,35 @@
 return {
   "nvim-mini/mini.nvim",
-  version = false, -- Use for stability; omit to use `main` branch for the latest features
+  version = false,
   event = "VeryLazy",
   config = function()
     require("mini.surround").setup({
-      -- Configuration here, or leave empty to use defaults
+      custom_surroundings = {
+        g = {
+          -- `input` is REQUIRED for the mapping to be accepted.
+          -- Bonus: This specific regex pattern means `sdg` (Surround Delete Generic)
+          -- will now correctly find and delete things like `Box<` and `>`!
+          input = { "[%w:]-<().-()>" },
+
+          output = function()
+            -- Prompt the user
+            local obj = vim.fn.input("Generic Type: ")
+
+            -- If you press Escape, cancel the surround
+            if obj == nil then
+              return nil
+            end
+
+            -- If you just hit Enter without typing, just wrap in <>
+            if obj == "" then
+              return { left = "<", right = ">" }
+            end
+
+            -- Otherwise, wrap in Type< >
+            return { left = obj .. "<", right = ">" }
+          end,
+        },
+      },
     })
   end,
 }
