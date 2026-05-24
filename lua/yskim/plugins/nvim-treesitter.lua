fd3efb168327bea6048e7ea7d884a5883791953e
Young Seo Kim <youngseokim7@gmail.com>
2026-05-24 15:55:58 +0900

    update depracated treesitter config

--- lua/yskim/plugins/nvim-treesitter.lua
+++ lua/yskim/plugins/nvim-treesitter.lua
@@ -1,44 +1,37 @@
 return {
   "nvim-treesitter/nvim-treesitter",
+  branch = "main", -- Explicitly target the new branch
   build = ":TSUpdate",
   dependencies = {
-    "windwp/nvim-ts-autotag", -- auto-close & rename HTML/JSX tags
+    "windwp/nvim-ts-autotag",
   },
   config = function()
-    local configs = require("nvim-treesitter.configs")
-    configs.setup({
-      ensure_installed = {
-        "c",
-        "lua",
-        "vim",
-        "vimdoc",
-        "cpp",
-        "python",
-        "css",
-        "javascript",
-        "typescript",
-        "tsx",
-        "html",
-        "markdown",
-        "go",
-        "markdown_inline",
-      },
-      sync_install = false,
-      auto_install = true,
-      ignore_install = {},
-      modules = {},
+    local langs = {
+      "c",
+      "lua",
+      "vim",
+      "vimdoc",
+      "cpp",
+      "python",
+      "css",
+      "javascript",
+      "typescript",
+      "tsx",
+      "html",
+      "markdown",
+      "go",
+      "markdown_inline",
+    }
 
-      highlight = { enable = true }, -- syntax highlighting
-      indent = { enable = true }, -- smart indentation
-      incremental_selection = { -- expand/shrink selections
-        enable = true,
-        keymaps = {
-          init_selection = "<CR>",
-          node_incremental = "<CR>",
-          scope_incremental = "<S-CR>",
-          node_decremental = "<BS>",
-        },
-      },
+    local install_group = vim.api.nvim_create_augroup("TreesitterInstall", { clear = true })
+    vim.api.nvim_create_autocmd("FileType", {
+      group = install_group,
+      pattern = langs,
+      callback = function(args)
+        pcall(vim.treesitter.start, args.buf)
+
+        vim.bo[args.buf].indentexpr = "v:lua.require'nvim-treesitter'.indentexpr()"
+      end,
     })
   end,
 }
