5b7f1f8d7cae74daae061d74561867f3dafdcb80
Young Seo Kim <youngseokim7@gmail.com>
2026-06-11 17:27:34 +0900

    enter-style completions

--- lua/yskim/plugins/cmp.lua
+++ lua/yskim/plugins/cmp.lua
@@ -23,17 +23,17 @@ return {
         end,
       },
       completion = {
-        completeopt = "menu,menuone,noinsert",
+        completeopt = "menu,menuone",
       },
       mapping = cmp.mapping.preset.insert({
         ["<C-b>"] = cmp.mapping.scroll_docs(-4),
         ["<C-f>"] = cmp.mapping.scroll_docs(4),
         ["<C-Space>"] = cmp.mapping.complete(),
         ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = false }),
+        ["<CR>"] = cmp.mapping.confirm({ select = true }),
         ["<Tab>"] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
+            cmp.confirm({ select = true })
           elseif luasnip.expand_or_locally_jumpable() then
             luasnip.expand_or_jump()
           else
